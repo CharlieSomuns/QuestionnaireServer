53ee7a2fbc096a4d551e850f08f8c0b5ac3153c2
mering <canhuayin@gmail.com>
2018-06-06 13:52:12 +0800

    问卷表单,功能变更:添加移动位置功能

--- src/app/customer/questionnaire-form/questionnaire-form.component.ts
+++ src/app/customer/questionnaire-form/questionnaire-form.component.ts
@@ -11,11 +11,22 @@ import { sessionstorage } from '../../utils/storage'
 export class QuestionnaireFormComponent implements OnInit {
     api = '/api/v1/customer_questionnaire'
     questionnaire_id: any
+    categorys = [
+        {
+            name: '单选题',
+            category: 'radio'
+        },
+        {
+            name: '多选选题',
+            category: 'select'
+        },
+    ]
     now = new Date()
     questionnaire: any = {
         title: '',
         deadline: this.now.getFullYear() + '-' + (this.now.getMonth() + 1) + '-' + this.now.getDate(),
-        quantity: 100
+        quantity: 100,
+        questions: []
     }
     constructor(private router: Router) {
         this.questionnaire_id = sessionstorage.get('questionnaire_id')
@@ -24,7 +35,62 @@ export class QuestionnaireFormComponent implements OnInit {
     ngOnInit() {
         this.get()
     }
+    up_question(index) {
+        console.log('up')
+        if (index <= 0) return;
+        let current = this.questionnaire.questions[index]
+        let pre = this.questionnaire.questions[index - 1]
+        this.questionnaire.questions[index] = pre
+        this.questionnaire.questions[index - 1] = current
+    }
+    down_question(index) {
+        console.log('down')
+        if (index >= this.questionnaire.questions.length - 1) return;
+        let current = this.questionnaire.questions[index]
+        let next = this.questionnaire.questions[index + 1]
+        this.questionnaire.questions[index] = next
+        this.questionnaire.questions[index + 1] = current
+    }
+    add_quesiton(category) {
+        this.questionnaire.questions.push(
+            {
+                title: '',
+                items: []
+            }
+        )
+    }
+    add_question_item(question) {
+        question.items.push({
+            content: ''
+        })
+    }
+    remove_question_item(question, index) {
+        question.items.splice(index, 1)
+    }
+    up_question_item(question, index) {
+        if (index <= 0) return;
+        let current = question.items[index]
+        let pre = question.items[index - 1]
+        question.items[index] = pre
+        question.items[index - 1] = current
+    }
+    down_question_item(question, index) {
+        if (index >= question.items.length - 1) return;
+        let current = question.items[index]
+        let next = question.items[index + 1]
+        question.items[index] = next
+        question.items[index + 1] = current
+    }
+    save_question(question) {
+        let ajax = new Ajax()
+        ajax.success = data => {
+        }
 
+    }
+
+    delete_question(index) {
+        this.questionnaire.questions.splice(index, 1)
+    }
     get() {
         let that = this
         let ajax = new Ajax()
